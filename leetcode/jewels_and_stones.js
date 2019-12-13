48590a6c9312983c345c7c148b77a8f8dce64848
Tess Neau <tessneau@gmail.com>
2019-12-13 15:49:11 -0500

    js jewels

---
+++ leetcode/jewels_and_stones.js
@@ -0,0 +1,8 @@
+var numJewelsInStones = function(J, S) {
+    let result = 0;
+    for(let i = 0; i < S.length; i++) {
+        if(J.indexOf(S.charAt(i)) >= 0)
+            result++;
+    }
+    return result;
+};
